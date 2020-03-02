e9165cb1e6b36acf8d4fa17fa8493127a0f5ac62
marcbarry <>
2020-03-02 18:24:20 +0000

    drop target version to netstandard1.1

--- MscVersion/Msi.cs
+++ MscVersion/Msi.cs
@@ -16,8 +16,8 @@ namespace MscVersion
         {
             var state = MsiQueryProductState($"{{{productCode}}}");
 
-            return state == InstallState.INSTALLSTATE_LOCAL ||
-                   state == InstallState.INSTALLSTATE_DEFAULT;
+            return state == InstallState.LOCAL ||
+                   state == InstallState.DEFAULT;
         }
 
         [DllImport("msi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
@@ -27,19 +27,19 @@ namespace MscVersion
     public enum InstallState
     {
         // ReSharper disable InconsistentNaming
-        INSTALLSTATE_NOTUSED = -7,  // component disabled
-        INSTALLSTATE_BADCONFIG = -6,  // configuration datacorrupt
-        INSTALLSTATE_INCOMPLETE = -5,  // installationsuspended or in progress
-        INSTALLSTATE_SOURCEABSENT = -4,  // run from source,source is unavailable
-        INSTALLSTATE_MOREDATA = -3,  // return bufferoverflow
-        INSTALLSTATE_INVALIDARG = -2,  // invalid functionargument
-        INSTALLSTATE_UNKNOWN = -1,  // unrecognized productor feature
-        INSTALLSTATE_BROKEN = 0,  // broken
-        INSTALLSTATE_ADVERTISED = 1,  // advertised feature
-        INSTALLSTATE_REMOVED = 1,  // component being removed(action state, not settable)
-        INSTALLSTATE_ABSENT = 2,  // uninstalled (or actionstate absent but clients remain)
-        INSTALLSTATE_LOCAL = 3,  // installed on local drive
-        INSTALLSTATE_SOURCE = 4,  // run from source, CD ornet
-        INSTALLSTATE_DEFAULT = 5,  // use default, local orsource
+        NOTUSED = -7,  // component disabled
+        BADCONFIG = -6,  // configuration datacorrupt
+        INCOMPLETE = -5,  // installationsuspended or in progress
+        SOURCEABSENT = -4,  // run from source,source is unavailable
+        MOREDATA = -3,  // return bufferoverflow
+        INVALIDARG = -2,  // invalid functionargument
+        UNKNOWN = -1,  // unrecognized productor feature
+        BROKEN = 0,  // broken
+        ADVERTISED = 1,  // advertised feature
+        REMOVED = 1,  // component being removed(action state, not settable)
+        ABSENT = 2,  // uninstalled (or actionstate absent but clients remain)
+        LOCAL = 3,  // installed on local drive
+        SOURCE = 4,  // run from source, CD ornet
+        DEFAULT = 5,  // use default, local orsource
     }
 }
